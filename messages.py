d4e93ad41d5e9b3fc0fe7fb9dc042e3020a5a3ce
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-05-30 16:01:40 +0530

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,5 @@
+class Translation(object):
+    START_TEXT = "** **"
+######################
+    HELP_USER = """**• Just Send Me A Short Link And Get Direct Link \n• Currently Supported Sites : Gplinks, DropLinks**"""
+    PROCESS_MSG = "**Processing...⏳**"
