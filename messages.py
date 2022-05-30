c181c196c8fc8e500cc50d7b7b99397fc4228df9
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-05-30 16:07:18 +0530

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = "** **"
+    START_TEXT = "**I Am Alive {message.chat.first_name}** \n**I Am Link Bypasser Bot, Just Send Me Short Link And Get Direct Link**"
 ######################
-    HELP_USER = """**• Just Send Me A Short Link And Get Direct Link \n• Currently Supported Sites : Gplinks, DropLinks**"""
+    HELP_TEXT = """**• Just Send Me A Short Link And Get Direct Link \n• Currently Supported Sites : Gplinks, DropLinks**"""
     PROCESS_MSG = "**Processing...⏳**"
