3dd0151ca0f731d4b58f667f4ab569fddb661670
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-06-19 19:38:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,6 +131,38 @@ async def droplink_bypass(url):
         return res.json()['url'].replace('\/','/')
     except: 
         return "An Error Occured "
+
+def rocklinks_bypass(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    if 'rocklinks.net' in url:
+        DOMAIN = "https://links.spidermods.in"
+    else:
+        DOMAIN = "https://rocklink.in"
+
+    url = url[:-1] if url[-1] == '/' else url
+
+    code = url.split("/")[-1]
+    if 'rocklinks.net' in url:
+        final_url = f"{DOMAIN}/{code}?quelle=" 
+    else:
+        final_url = f"{DOMAIN}/{code}"
+
+    resp = client.get(final_url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "Incorrect Link"
+    
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(5)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: 
+        return "An Error Occured "
          
 
 
