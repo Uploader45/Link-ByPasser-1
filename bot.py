e184800164ded86f8a470d5247b7be6fe0feb0a0
kangScripter <67798221+kangScripter@users.noreply.github.com>
2022-06-20 20:03:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def link_handler(bot, message):
     try:
         short_link = await gplinks_bypass(link)
         mess = await message.reply_text("**Bypassing...⏳**",quote=True)
-        await mess.edit_text(f"**Bypassed URL** : {short_link} \n\n ©cc:{message.from_user.first_name}",disable_web_page_preview=True)
+        await mess.edit_text(f"**Bypassed URL** : {short_link} \n\n ©cc:{message.from_user.mention}",disable_web_page_preview=True)
     except Exception as e:
         await mess.edit_text(f"**Error** : {e}")
   elif 'droplink.co' in link:
