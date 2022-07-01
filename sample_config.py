dcd61c371fc43442f778ab44a175ca5515c31b8a
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-01 18:19:08 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class Config(object):
 
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1300445326").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1478357602").split())
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     LOG_CHAN = int(os.environ.get("LOG_CHAN", "-1001145342921"))
     UPDATE_CHANNEL = "DevilBotz"
