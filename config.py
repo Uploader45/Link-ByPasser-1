2c2cd9ba8fc2a0289b18e6c39c281ffa3b7c0153
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-01 19:32:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config(object):
+
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1478357602").split())
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    LOG_CHAN = int(os.environ.get("LOG_CHAN", "-1001145342921"))
+    UPDATE_CHANNEL = "DevilBotz"
+    USERNAME = os.environ.get("USERNAME", "Stay007") 
+    BANNED_USERS = os.environ.get("BANNED_USERS", "")
+    ADL_BOT_RQ = {}
