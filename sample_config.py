85929cc18f44f363e8b584e2d6831a888b9b4ffb
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-01 20:01:51 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,5 +7,5 @@ class Config(object):
     LOG_CHAN = int(os.environ.get("LOG_CHAN", "-1001145342921"))
     UPDATE_CHANNEL = "DevilBotz"
     USERNAME = os.environ.get("USERNAME", "Stay007") 
-    BANNED_USERS = os.environ.get("BANNED_USERS", "")
+    set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     ADL_BOT_RQ = {}
