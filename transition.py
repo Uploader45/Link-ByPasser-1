702fa499223533e55124e2be09cf0ae821cc4b83
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-02 13:00:55 +0530

    Update transition.py

--- transition.py
+++ transition.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """**Hai {}, \n**I Am Link Bypasser Bot, Just Send Me Short Link And Get Direct Link**"""
+    START_TEXT = """**Hai {}, **I Am Link Bypasser Bot, Just Send Me Short Link And Get Direct Link**"""
     HELP_TEXT = """**• Just Send Me A Short Link And Get Direct Link \n• Currently Supported Sites : Gplinks, DropLinks, Rocklinks** \nCammand Usage : /bpp Link"""
     PROCESS_MSG = "**Processing...⏳**"
     ABOUT_TEXT = "<b>Simple Project \nVersion = 1.0</b>"
