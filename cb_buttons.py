9ba233ca14c847f9742f272ee797b6da4c6f313f
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-02 11:19:17 +0530

    Update cb_buttons.py

--- cb_buttons.py
+++ cb_buttons.py
@@ -12,7 +12,16 @@ from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from help_text import help, start, about
 
-Client=bot
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+CRYPT = 'ajRmMzd2ZEdxL055ZC9vMHlwNGZwZUE4Zm9MSzFUVDRETU9ESm4xU1lqcz0%3D'
+#API_KEY = environ.get('API_KEY')
+
+bot = Client('LinkByPass bot',
+             api_id= "1543212",
+             api_hash= "d47de4b25ddf79a08127b433de32dc84",
+             bot_token= "5462389029:AAHiRZVyr-WL2e80y0wz-e7hd9oTOIlM5iY")
 
 
 @bot.on_callback_query()
