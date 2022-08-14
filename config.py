7fa1c933642d0382518e9680100a52d359355fe7
Uploader45 <96682675+Uploader45@users.noreply.github.com>
2022-08-14 12:30:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-import os
+import os 
 
 class Config(object):
 
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "579974245").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1478357602").split())
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     LOG_CHAN = int(os.environ.get("LOG_CHAN", "-1001145342921"))
     UPDATE_CHANNEL = "DevilBotz"
-    USERNAME = os.environ.get("USERNAME", "KnightRanger") 
+    USERNAME = os.environ.get("USERNAME", "Stay007") 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     ADL_BOT_RQ = {}
